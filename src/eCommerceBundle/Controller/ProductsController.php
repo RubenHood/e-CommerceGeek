dca9851afc6ae91437a35b62d4782d9bcfced383
Ruben__hood <rubenconb88@gmail.com>
2019-07-03 01:31:58 +0200

    Add get all method on Controller

--- src/eCommerceBundle/Controller/ProductsController.php
+++ src/eCommerceBundle/Controller/ProductsController.php
@@ -4,14 +4,23 @@ namespace eCommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use eCommerceBundle\Entity\Product;
 
 class ProductsController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function getAllAction()
     {
-        return $this->render('@eCommerce/Default/all_products.html.twig');
+        //recuperamos el entiti manager
+        $em = $this->getDoctrine()->getManager();
+
+        //obtenemos la referencia al repositorio
+        $repository = $em->getRepository(Product::class);
+
+        $products = $repository->findAll();
+
+        return $this->render("@eCommerce/Default/all_products.html.twig", ["products" => $products]);
     }
 }
