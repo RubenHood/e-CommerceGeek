764a42f4d42228daab4cd4c930ff9cacc6730451
Ruben__hood <rubenconb88@gmail.com>
2019-07-05 12:31:03 +0200

    Generating User Crud

--- src/eCommerceBundle/Controller/ProductsController.php
+++ src/eCommerceBundle/Controller/ProductsController.php
@@ -5,6 +5,10 @@ namespace eCommerceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use eCommerceBundle\Entity\Product;
+use eCommerceBundle\Entity\User;
+use eCommerceBundle\Form\UserType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\BrowserKit\Request;
 
 class ProductsController extends Controller
 {
@@ -21,7 +25,7 @@ class ProductsController extends Controller
 
         $products = $repository->findAll();
 
-        return $this->render("@eCommerce/Default/all_products.html.twig", ["products" => $products]);
+        return $this->render("@eCommerce/Product/all_products.html.twig", ["products" => $products]);
     }
 
     /**
@@ -30,29 +34,47 @@ class ProductsController extends Controller
     public function getOneAction(Product $product)
     {
         //recuperamos el entiti manager
-        // $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
 
         // //obtenemos la referencia al repositorio
-        // $repository = $em->getRepository(Product::class);
+        $repository = $em->getRepository(Product::class);
 
-        // $products = $repository->find($product->id);
+        $products = $repository->find($product);
 
-        return $this->render("@eCommerce/Default/one_products.html.twig", ["product" => $product]);
+        return $this->render("@eCommerce/Product/one_products.html.twig", ["product" => $product]);
     }
 
     /**
-     * @Route("/login", name="login")
+     * @Route("/login", name="login", methods={"GET", "POST"})
      */
-    public function loginAction()
+    public function loginAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
-        //recuperamos el entiti manager
-        $em = $this->getDoctrine()->getManager();
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
 
-        //obtenemos la referencia al repositorio
-        $repository = $em->getRepository(Product::class);
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
 
-        $products = $repository->findAll();
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
 
-        return $this->render("@eCommerce/Default/all_products.html.twig", ["products" => $products]);
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('showAll');
+        }
+
+        return $this->render('@eCommerce/User/new.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/eCommerceBundle/Controller/UserController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace eCommerceBundle\Controller;
+
+use eCommerceBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use eCommerceBundle\Form\UserType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * User controller.
+ *
+ * @Route("/user")
+ */
+class UserController extends Controller
+{
+    /**
+     * 
+     *
+     * @Route("/", name="user_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('eCommerceBundle:User')->findAll();
+
+        return $this->render('@eCommerce/User/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+    /**
+     *
+     * @Route("/new", name="user_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('user_show', array('id' => $user->getId()));
+        }
+
+        return $this->render('@eCommerce/User/new.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * 
+     *
+     * @Route("/{id}", name="user_show")
+     * @Method("GET")
+     */
+    public function showAction(User $user)
+    {
+        $deleteForm = $this->createDeleteForm($user);
+
+        return $this->render('@eCommerce/User/show.html.twig', array(
+            'user' => $user,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/{id}/edit", name="user_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, User $user)
+    {
+        $deleteForm = $this->createDeleteForm($user);
+        $editForm = $this->createForm('eCommerceBundle\Form\UserType', $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+        }
+
+        return $this->render('@eCommerce/User/edit.html.twig', array(
+            'user' => $user,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/{id}", name="user_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, User $user)
+    {
+        $form = $this->createDeleteForm($user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('user_index');
+    }
+
+    /**
+     * @param User $user The user entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(User $user)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('user_delete', array('id' => $user->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
