6adf79df42b8bdb459df0698f919fc1773d4861a
Ruben__hood <rubenconb88@gmail.com>
2019-07-11 10:57:18 +0200

    Create Cart entity

---
+++ src/eCommerceBundle/Entity/Cesta.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace eCommerceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cesta
+ *
+ * @ORM\Table(name="cesta")
+ * @ORM\Entity(repositoryClass="eCommerceBundle\Repository\CestaRepository")
+ */
+class Cesta
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idUser", type="integer")
+     */
+    private $idUser;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idProduct", type="integer")
+     */
+    private $idProduct;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="cantidad", type="integer")
+     */
+    private $cantidad;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idUser
+     *
+     * @param integer $idUser
+     *
+     * @return Cesta
+     */
+    public function setIdUser($idUser)
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get idUser
+     *
+     * @return int
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * Set idProduct
+     *
+     * @param integer $idProduct
+     *
+     * @return Cesta
+     */
+    public function setIdProduct($idProduct)
+    {
+        $this->idProduct = $idProduct;
+
+        return $this;
+    }
+
+    /**
+     * Get idProduct
+     *
+     * @return int
+     */
+    public function getIdProduct()
+    {
+        return $this->idProduct;
+    }
+
+    /**
+     * Set cantidad
+     *
+     * @param integer $cantidad
+     *
+     * @return Cesta
+     */
+    public function setCantidad($cantidad)
+    {
+        $this->cantidad = $cantidad;
+
+        return $this;
+    }
+
+    /**
+     * Get cantidad
+     *
+     * @return int
+     */
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
+}
+
